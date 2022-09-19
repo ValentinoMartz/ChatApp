2aa6ee5b4ce7f7e02f9f509c13bab75a1a0c160c
ValentinoMartz <valentinomartinez55@gmail.com>
2022-09-19 18:58:49 -0300

    firebase half

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import Register from "./pages/Register";
 import "./style.scss";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+
 function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+  return <Register />;
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCVRyrQTq8ir3KJezEG5z6bwXnQfdcFigE", //puedo ponerlo en un env dsp
+  authDomain: "chat-e4988.firebaseapp.com",
+  projectId: "chat-e4988",
+  storageBucket: "chat-e4988.appspot.com",
+  messagingSenderId: "140381402163",
+  appId: "1:140381402163:web:cfc52b9969bb60d1601f46",
+  measurementId: "G-H9HDHSM9CB",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore();
