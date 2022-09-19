928e7919940cd1b172bb789746d6ac3203e60f89
ValentinoMartz <valentinomartinez55@gmail.com>
2022-09-19 11:45:40 -0300

    styles done

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="messageinfo">
+        <img
+          src="https://images.pexels.com/photos/2755165/pexels-photo-2755165.jpeg?auto=compress&cs=tinysrgb&w=1600"
+          alt=""
+        />
+        <span>just now</span>
+      </div>
+      <div className="messagecontent">
+        <p>Hello</p>
+        <img
+          src="https://images.pexels.com/photos/2755165/pexels-photo-2755165.jpeg?auto=compress&cs=tinysrgb&w=1600"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
